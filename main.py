4728715c4b3442532203b426c96c7d6aa95da1b0
Selim <hselimozturkgmail.com>
2018-03-04 22:57:33 +0300

    proximity handled

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def go():
 
 			for news_article in soup.find_all('reuters'):
 
-				news_id = int(news_article.get('newid'))
+				doc_id = int(news_article.get('newid'))
 				news_text = ''
 
 				if news_article.title:
@@ -29,15 +29,15 @@ def go():
 				if news_article.body:
 					news_text += news_article.body.string.lower()
 
-				tokenize(news_text, news_id)
+				tokenize(news_text, doc_id)
 				
 
-	create_index_files(dict)
+	create_index_files()
 				
 
 				
 			
-def tokenize(news_text, news_id):
+def tokenize(news_text, doc_id):
 	f = open('stopwords.txt', 'r')
 	stopwords = f.read()
 	f.close()
@@ -46,13 +46,19 @@ def tokenize(news_text, news_id):
 	# [\w] means any alphanumeric characters [a-zA-Z0-9_]
 	word_list = re.sub("[^\w]", " ",  news_text).split()
 
-	for word in word_list:
+	for position, word in enumerate(word_list):
 		if word not in stopwords:
 			token = stem(word)
 			if token in dict:
-				dict[token].append(news_id)
+				if doc_id in dict[token]:
+					temp = dict[token][doc_id]
+					temp.append(position)
+					dict[token][doc_id] = temp				
+				else:
+					dict[token][doc_id] = [position]
 			else:
-				dict[token] = [news_id]
+				dict[token] = {}
+				dict[token][doc_id] = [position]
 
 
 def stem(word):
@@ -60,18 +66,16 @@ def stem(word):
 	return p.stem(word,0,len(word)-1).encode('utf8')
 
 
-def create_index_files(dict):
+def create_index_files():
 	postings_list = {}
-
 	for i, key in enumerate(sorted(dict.keys())):
-		postings_list[i] = sorted(dict[key])
+		postings_list[i] = dict[key]
 		dict[key] = i
-	
+
 	with open('dictionary.txt', 'w') as f:
 		f.write(str(dict))
 	with open('postings_list.txt', 'w') as f:
 		f.write(str(postings_list))
-
 
 			
 if __name__ == "__main__":
